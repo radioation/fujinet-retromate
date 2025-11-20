ed67a5379536a6c541310b52d23e99a2756f04d0
radioation <greggallardo@gmail.com>
2025-11-19 23:09:00 -0600

    converting over to fujinet. experimental

--- src/atari/platAtarinet.c
+++ src/atari/platAtarinet.c
@@ -8,14 +8,27 @@
  */
 
 #include <atari.h>
-#include <ip65.h>
+
 #include <string.h>
 
 #include "../global.h"
 
 #include "platAtari.h"
 
+#include "fujinet-network.h"
+
 #pragma code-name(push, "SHADOW_RAM")
+
+
+char devicespec[256];    // Buffer for the device spec
+uint16_t bytes_waiting;
+uint8_t conn_status;
+uint8_t err;
+uint8_t rxbuf[8192];     // Receive buffer
+
+char* fn_strerror( uint8_t ) {
+    return "error";
+}
 
 /*-----------------------------------------------------------------------*/
 static int plat_net_make_ascii(const char *text) {
@@ -35,17 +48,24 @@ static int plat_net_make_ascii(const char *text) {
 
 /*-----------------------------------------------------------------------*/
 void plat_net_init() {
-    if (ip65_init(ETH_INIT_DEFAULT)) {
+    //if (ip65_init(ETH_INIT_DEFAULT)) {  // true if error, false otherwise.
+    int8_t ret = network_init();
+     
+    if ( ret ) { // returns status/error FN_ERR_* values  FN_ERR_OK ==0x00, so non zero is error
         log_add_line(&global.view.terminal, "Initializing Network", -1);
         plat_draw_log(&global.view.terminal, 0, 0, false);
-        app_error(true, ip65_strerror(ip65_error));
+        //app_error(true, ip65_strerror(ip65_error));
+        app_error(true, fn_strerror(ret));
     }
 }
+
 
 /*-----------------------------------------------------------------------*/
 void plat_net_connect(const char *server_name, int server_port) {
     uint32_t server;
+    int8_t ret = 0;
 
+/* internal to fujinet
     log_add_line(&global.view.terminal, "Obtaining IP address", -1);
     plat_draw_log(&global.view.terminal, 0, 0, false);
     if (dhcp_init()) {
@@ -59,11 +79,17 @@ void plat_net_connect(const char *server_name, int server_port) {
         app_error(false, ip65_strerror(ip65_error));
     }
 
+*/
     log_add_line(&global.view.terminal, "Connecting to server", -1);
     plat_draw_log(&global.view.terminal, 0, 0, false);
-    if (tcp_connect(server, server_port, fics_tcp_recv)) {
-        app_error(false, ip65_strerror(ip65_error));
+    //if (tcp_connect(server, server_port, fics_tcp_recv)) {
+    //    app_error(false, ip65_strerror(ip65_error));
+    //}
+    ret = network_open( devicespec, OPEN_MODE_RW, OPEN_TRANS_NONE );
+    if( ret ) {
+        app_error(true, fn_strerror(ret));
     }
+
 
     log_add_line(&global.view.terminal, "Logging in, please be patient", -1);
     plat_draw_log(&global.view.terminal, 0, 0, false);
@@ -71,27 +97,42 @@ void plat_net_connect(const char *server_name, int server_port) {
 
 /*-----------------------------------------------------------------------*/
 void plat_net_disconnect() {
-    tcp_close();
+    //tcp_close();
+    network_close(devicespec);
 }
 
 /*-----------------------------------------------------------------------*/
 bool plat_net_update() {
+/*
     if (ip65_process()) {
         // I am not sure what erors could be returned here
         if (ip65_error >= IP65_ERROR_PORT_IN_USE) {
             return 1;
         }
     }
+
+*/
+    network_status( devicespec, &bytes_waiting, &conn_status, &err );
+    if( conn_status && bytes_waiting ) {
+        network_read( devicespec, rxbuf, bytes_waiting );
+    } 
+
+    
     return 0;
 }
 
 /*-----------------------------------------------------------------------*/
 void plat_net_send(const char *text) {
     log_add_line(&global.view.terminal, text, -1);
+/*
     tcp_send((unsigned char *)atari.send_buffer, plat_net_make_ascii(text));
     // Don't send \x0a in he buffer, send seprately.  I don't know why it
     // improves stability, but it absolutely does
     tcp_send((unsigned char *)"\x0a", 1);
+*/
+
+    network_write( devicespec, (unsigned char *)atari.send_buffer, plat_net_make_ascii(text));
+    network_write( devicespec, (unsigned char *)"\x0a", 1 );
 }
 
 /*-----------------------------------------------------------------------*/
