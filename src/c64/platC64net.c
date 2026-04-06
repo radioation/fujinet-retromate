7580b8163653868b4692a115a57e2f2a8c7fe0b1
radioation <greggallardo@gmail.com>
2026-04-06 11:40:56 -0500

    updating network

--- src/c64/platC64net.c
+++ src/c64/platC64net.c
@@ -8,7 +8,9 @@
  */
 
 #include <c64.h>
+#include <cbm.h>
 #include <string.h>
+#include <stdlib.h>
 
 #include "../global.h"
 
@@ -21,7 +23,77 @@
 
 char url[64];
 unsigned char rxbuf[1518]; // ip_65 eth_buffer.s .res 1518 &  drivers/ethernetcombo.s drivers/ethernet.s stax #1518
+uint8_t res;
+uint16_t bytes_waiting;
+//uint8_t conn_status;
+//uint8_t err;
+//uint8_t bytes_read;
+uint8_t settrans_cmd[] = "settrans,2,4";
+uint8_t nw_status_cmd[] = "statusb,2";
+extern void ihsetup();
+bool trip = false;
+bool connected = false;
+uint16_t network_bw;
+uint8_t network_conn;
+uint8_t network_error;
 
+/*-----------------------------------------------------------------------*/
+char* cbm_strerror( uint8_t e ) {
+     switch(e) // from cbm.h 
+     { 
+          case 0:
+              return "OK";
+          case 1:
+              return "Too many files";
+          case 2:
+              return "File open";
+          case 3:
+              return "File not open";
+          case 4:
+              return "File not found";
+          case 5:
+              return "Device not present";
+          case 6:
+              return "Not input-file";
+          case 7:
+              return "Not output-file";
+          case 8:
+              return "Missing file-name";
+          case 9:
+              return "Illegal device-number";
+          case 10:
+              return "STOP-key pushed";
+          case 11:
+              return "General I/O-error";
+          default:
+              return "Unknown";
+     }
+}
+
+/*-----------------------------------------------------------------------*/
+uint8_t get_network_status(uint16_t *bw, uint8_t *c, uint8_t *err) {
+    uint8_t status[4];
+    if (cbm_write(CMD, (char*)nw_status_cmd, strlen( nw_status_cmd )) != strlen(nw_status_cmd))  {
+        return 1;
+    }
+
+    // now the command channel was updated with the correct data channel, let's do a read on the command channel and set the values from it
+    if (cbm_read(CMD, status, 4) != 4) {
+        return 1;
+    }
+
+    if (bw) {
+        *bw = (uint16_t)(status[1] << 8) | status[0]; // Combine the first two bytes for bw
+    }
+    if (c) {
+        *c = status[2]; // The third byte is directly assigned to c
+    }
+    if (err) {
+        *err = status[3]; // The fourth byte is directly assigned to err
+    }
+
+    return 0;
+}
 
 /*-----------------------------------------------------------------------*/
 static int plat_net_make_ascii(const char *text) {
@@ -50,17 +122,20 @@ static int plat_net_make_ascii(const char *text) {
 
 /*-----------------------------------------------------------------------*/
 void plat_net_init() {
-    if( cbm_open( CMD,DEV,CMD, "") ) {
+    ihsetup();
+    res =  cbm_open( CMD,DEV,CMD, ""); // return 0 if openning was successful
+    if( res ) {
         log_add_line(&global.view.terminal, "Initializing Network", -1);
         plat_draw_log(&global.view.terminal, 0, 0, false);
-        app_error(true, ip65_strerror(ip65_error));
+        app_error(true, cbm_strerror(res));
     }
+    cbm_write(CMD,settrans_cmd,sizeof(settrans_cmd));
 }
 
 /*-----------------------------------------------------------------------*/
 void plat_net_connect(const char *server_name, int server_port) {
 
-    strcpy( url, "TELNET://");
+    strcpy( url, "telnet://");
     strcat( url, server_name );
     strcat( url, ":" );
     itoa( server_port,  url+10 + strlen(server_name), 10 );
@@ -69,9 +144,9 @@ void plat_net_connect(const char *server_name, int server_port) {
     plat_draw_log(&global.view.terminal, 0, 0, false);
 
 
-    res = cbm_open( LFN, DEV, SAN, device);
+    res = cbm_open( LFN, DEV, SAN, url);
     if ( res ) {
-        app_error(false, fn_strerror(res));
+        app_error(false, cbm_strerror(res));
     }
 
     log_add_line(&global.view.terminal, "Logging in, please be patient", -1);
@@ -86,11 +161,25 @@ void plat_net_disconnect() {
 
 /*-----------------------------------------------------------------------*/
 bool plat_net_update() {
-
-    if( network_status( devicespec, &bytes_waiting, &conn_status, &err ) == FN_ERR_OK ) {
+    int16_t retval;
+    uint8_t *bufptr;
+    if( trip ) {
+        retval = cbm_read( LFN, rxbuf, sizeof( rxbuf ) );
+        if( retval < 0 ) {
+            get_network_status( &network_bw, &network_conn, & network_error );
+            connected = network_conn;
+        } else {
+            fics_tcp_recv( rxbuf, retval );
+            return 0;
+        }
+    }
+  
+    return connected;
+    /*
+    if( get_network_status( &bytes_waiting, &conn_status, &err ) == 0 ) {
         if( conn_status  ){
             if(  bytes_waiting ) {
-                bytes_read = network_read( devicespec, rxbuf, bytes_waiting < sizeof( rxbuf ) ? bytes_waiting : sizeof( rxbuf ) );
+                bytes_read = cbm_read( LFN, rxbuf, bytes_waiting < sizeof( rxbuf ) ? bytes_waiting : sizeof( rxbuf ) );
                 if( bytes_read < 0 ) {
                     return 1;
                 }
@@ -103,14 +192,16 @@ bool plat_net_update() {
     }
     // Got an error if we're here. network_status returns either FN_ERR_OK or FN_ERR_IO_ERROR.
     return 1;
+    */
+
+
 }
 
 /*-----------------------------------------------------------------------*/
 void plat_net_send(const char *text) {
-    int len = strlen(text);
     log_add_line(&global.view.terminal, text, -1);
     //tcp_send((unsigned char *)c64.send_buffer, plat_net_make_ascii(text));
-    cbm_write( LFN, (unsigned char *)text, len );
+    cbm_write( LFN, (unsigned char *)c64.send_buffer, plat_net_make_ascii( text ) );
 }
 
 /*-----------------------------------------------------------------------*/
